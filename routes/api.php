abd9e8c79df356829cb3110eb74a66131c9aed81
vfporto <2643007+vfporto@users.noreply.github.com>
2019-05-20 00:36:48 -0300

    login funcionando

--- routes/api.php
+++ routes/api.php
@@ -17,6 +17,7 @@ Route::middleware('auth:api')->get('/user', function (Request $request) {
     return $request->user();
 });
 
+Route::get('/', function () {return "TESTE";});
 Route::post('login', 'ApiController@login');
 Route::post('register', 'ApiController@register');
 
